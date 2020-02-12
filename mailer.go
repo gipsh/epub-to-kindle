4f5d1db6bd6ffcb8f356037217b363df688593ef
gipsh <gipshm@gmail.com>
2020-02-12 15:52:27 -0300

    validates @kindle.com domain on mail

--- mailer.go
+++ mailer.go
@@ -2,15 +2,20 @@ package main
 
 import (
     "fmt"
+    "strings"
 
     "crypto/tls"
 
     "gopkg.in/gomail.v2"
    // "github.com/aws/aws-sdk-go/service/ses"
+    "github.com/badoux/checkmail"
 )
 
 func Send_Mail(ebook EBook, cfg Config) {
 	fmt.Println("Sending email")
+	if !ValidateAddress(ebook.Email) {
+		return
+	}
 
 	d := gomail.NewDialer(cfg.SMTP.Host, cfg.SMTP.Port, cfg.SMTP.User, cfg.SMTP.Pass)
 	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
@@ -23,8 +28,28 @@ func Send_Mail(ebook EBook, cfg Config) {
 	m.Attach(ebook.Mobi)
 
 	if err := d.DialAndSend(m); err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
 
 }
 
+
+func ValidateAddress(address string) bool  {
+
+		err := checkmail.ValidateFormat(address)
+		if err != nil {
+			fmt.Println("format not valid")
+			return false 
+		}
+
+	        components := strings.Split(address, "@")
+    		_, domain := components[0], components[1]
+		if domain !=  "kindle.com" {
+			fmt.Println("domain is not kindle.com")
+			return false
+		}
+	
+		return true
+
+
+}
